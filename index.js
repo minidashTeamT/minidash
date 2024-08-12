5d7e72676f431928f830aee8fa44bcea7d4bb930
JS LEE <109492059+JSLEE753@users.noreply.github.com>
2024-08-12 18:42:04 +0900

    Add source codes

---
+++ index.js
@@ -0,0 +1,5 @@
+export { checkedArr } from "./src/checkedArr"
+export { findOddNums } from "./src/findOddNums"
+export { findEvenNums } from "./src/findEvenNums"
+export { capitalize } from "./src/capitalize"
+
